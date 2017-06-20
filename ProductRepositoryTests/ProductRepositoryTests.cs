66708d0d696fc201ba74647506895e331f70fbf9
nik <nik@DK01WV6063>
2017-06-20 19:52:12 +0200

    Task 3: Unit tests

---
+++ ProductRepositoryTests/ProductRepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperShop.Product;
+using SuperShop.Product_repository;
+
+namespace ProductRepositoryTests {
+
+    [TestClass]
+    public class ProductRepositoryTests {
+        [TestMethod]
+        public void TestRetrieve() {
+            Product product = ProductRepository.Retrieve(1);
+            Assert.IsNotNull(product);
+        }
+
+        [TestMethod]
+        public void TestRetrieveList() {
+            List<Product> productList = ProductRepository.Retrieve();
+            Assert.IsNotNull(productList);
+        }
+
+        [TestMethod]
+        public void TestDeleteProduct() {
+            bool deletionIsOk = ProductRepository.DeleteProduct(1,5);
+            Assert.Equals(false, deletionIsOk);
+        }
+
+        [TestMethod]
+        public void TestSaveProduct()
+        {
+            List<Product> productList = new List<Product>();
+            productList.Add(new FoodProducts(1));
+            productList.Add(new FoodProducts(2));
+            productList.Add(new FoodProducts(3));
+            Assert.AreEqual(true, ProductRepository.SaveProducts(productList));
+        }
+    }
+}
--- SuperShop/Product repository/ProductRepository.cs
+++ SuperShop/Product repository/ProductRepository.cs
@@ -5,31 +5,37 @@ using SuperShop.Product;
 
 namespace SuperShop.Product_repository
 {
-    public  class ProductRepository {
+    public class ProductRepository {
         public static Product.Product Retrieve(int productID) {
-            return new FoodProducts(productID);
+            return new FoodProducts(1);
         }
 
-        public List<Product.Product> Retreive() {
+        public static List<Product.Product> Retrieve() {
             return new List<Product.Product>();
         }
 
-        public static void SaveProducts(List<Product.Product> Products) {
+        public static bool SaveProducts(List<Product.Product> Products) {
+            return true;
             foreach (Product.Product product in Products)
             {
                 Console.WriteLine(product.ProductID + product.InstanceCount);
             }
         }
-        public static void DeleteProduct(int productId, int instanceCount) {
+
+        public static bool DeleteProduct(int productId, int instanceCount) {
             Product.Product productToDelete = Retrieve(productId);
             if (productToDelete.InstanceCount >= instanceCount) {
                 productToDelete.InstanceCount -= instanceCount;
+                return true;
             }
-            else { Console.WriteLine("You can't delete more items then you have");}
+            else {                
+                Console.WriteLine("You can't delete more items then you have");
+                return false;
+            }
         }
+
         public static void UpdateProduct(int productId, Product.Product product) {
             Product.Product productToDelete = Retrieve(productId);
-
         }
     }
 }
